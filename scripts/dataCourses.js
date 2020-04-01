facfba3524577a971b691c5eb421a9105ecdcde9
unknown <ne.calero@uniandes.edu.co>
2020-04-01 16:06:58 -0500

    Cambio imagen

---
+++ scripts/dataCourses.js
@@ -0,0 +1,10 @@
+import { Course } from './Course.js';
+export var dataCourses = [
+    new Course("Desarrollo de Sw", "Jhonatan Alarcon", 3),
+    new Course("Sistemas Transaccionales", "German Bravo", 3),
+    new Course("Infraestructura Computacional", "Harlod Castro", 3),
+    new Course("TI en las Organizaciones", "Andrea Herrera", 3),
+    new Course("Lenguajes & Maquinas", "Silvia Takahashi", 3),
+    new Course("Pensamiento Computacional", "Pablo Figueroa", 2),
+    new Course("Calculo Integral", "Jean Carlos Cortissoz", 3)
+];
